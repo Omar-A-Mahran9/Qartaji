c5bd2b43155da2d4fb23b617eeef1e1b1dc7728f
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-02-19 23:16:20 +0200

    Removed sensitive data from commit history

--- app/Models/Withdraw.php
+++ app/Models/Withdraw.php
@@ -2,20 +2,13 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
 use App\Traits\HasTranslations;
->>>>>>> 6e9408d1d7ed1293240c4a9370795c0ca8120470
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Withdraw extends Model
 {
-<<<<<<< HEAD
-    use HasFactory;
-=======
     use HasFactory,HasTranslations;
->>>>>>> 6e9408d1d7ed1293240c4a9370795c0ca8120470
 
     protected $guarded = ['id'];
 
